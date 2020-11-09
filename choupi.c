302f9532afcb979e06123e4fcf44b0184ff06ae5
vivishu <kadiegui1999@gmail.com>
2020-11-09 15:48:39 +0000

    mon premier depot git

---
+++ choupi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int i;
+ printf("entrer 10:\n");
+  scanf("%d",&i);
+ 
+  {
+  printf ("saisir la valeur de %d\n",i);
+  i=i+1;
+  }
+  while(i<=10);
+return 0;
+  }   
