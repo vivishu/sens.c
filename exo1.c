90e2d0af447447c5d467a8a52d19e95f5850cb02
vivishu <kadiegui1999@gmail.com>
2020-11-10 09:08:06 +0000

    enregistrement

---
+++ exo1.c
@@ -0,0 +1,12 @@
+
+int main(){
+    float a;
+    int b;
+    int p;
+    printf("saisir un reel a");
+    scanf("%f",&a);
+    printf("saisir un reel b");
+    scanf("%f",&b);
+    printf("%fp=%fa*%fb",p,a,b);
+    return 0;
+}
